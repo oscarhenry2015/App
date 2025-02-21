204cf90838b274c0caa8624ad453cb8dc3d321ae
oscar <oscarhenry2015@gmail.com>
2025-02-21 15:38:41 +0000

    Added basic logo display setup

---
+++ arm_template_deployment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Login to Azure (if not already logged in)
+#az login --tenant 72b7ff39-ea55-4d53-924c-80758b5dba9b
+
+# Create a resource group (if not already created)
+az group create --name Breakfast_Club_RG --location eastus2
+
+# Deploy PostgreSQL to Azure
+az deployment group create \
+  --name postgreSQLDeployment \
+  --resource-group Breakfast_Club_RG \
+  --template-file templates/PostgreSQLTemplate.json \
+  --parameters parameters/PostgreSQLParameters.json \
+  --parameters administratorLoginPassword="P@ss1234"
+
+# Deploy WebApp and App Service Plan to Azure
+az deployment group create \
+  --name WebAppDeployment \
+  --resource-group Breakfast_Club_RG \
+  --template-file templates/WebAppTemplate.json \
+  --parameters parameters/WebAppParameters.json \
